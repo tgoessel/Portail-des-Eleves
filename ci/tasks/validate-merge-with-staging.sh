e9de412055c62dade6135d322bec3a64c1cda128
Florian Veaux <florian.veaux@mines-paristech.fr>
2018-06-11 19:55:20 -0400

    Initial CI

---
+++ ci/tasks/validate-merge-with-staging.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# http://redsymbol.net/articles/unofficial-bash-strict-mode/
+set -euxo pipefail
+IFS=$'\n\t'
+
+git fetch --quiet --no-tags origin master
+git fetch --no-tags --force origin staging:staging
+git diff
+git checkout staging
+git config --global user.email "gitlag@mines-paris.eu"
+git config --global user.name "GITLAB CI"
+env EDITOR=cat git merge --no-ff "$CI_COMMIT_SHA"
+
